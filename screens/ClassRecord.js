276da43876f68fd63d3599ffef5a5e4c264db7de
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2022-06-14 11:11:04 +0600

    interval modified

--- screens/ClassRecord.js
+++ screens/ClassRecord.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react'
-import { Text, View,Button,TouchableOpacity ,Platform} from 'react-native'
+import React, {Component} from 'react';
+import {Text, View, Button, TouchableOpacity, Platform} from 'react-native';
 
-import {NativeModules} from 'react-native';
-const RNFetchBlob = NativeModules.RNFetchBlob
 import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 const audioRecorderPlayer = new AudioRecorderPlayer();
 
@@ -22,16 +20,10 @@ export class ClassRecord extends Component {
     this.audioRecorderPlayer.setSubscriptionDuration(0.09); // optional. Default is 0.1
   }
 
-  //========== main onRecord function========// 
-
   onStartRecord = async () => {
-    
-
-
-
-  // const path='/data/user/0/com.recording/cache/sound.mp'
-    const result = await this.audioRecorderPlayer.startRecorder(path);
-    this.audioRecorderPlayer.addRecordBackListener((e) => {
+    // const path = '/data/user/0/com.recording/cache/sound.mp4';
+    const result = await this.audioRecorderPlayer.startRecorder();
+    this.audioRecorderPlayer.addRecordBackListener(e => {
       this.setState({
         recordSecs: e.currentPosition,
         recordTime: this.audioRecorderPlayer.mmssss(
@@ -40,14 +32,10 @@ export class ClassRecord extends Component {
       });
       return;
     });
-    console.log('uri is:',result);
-
+    console.log('uri is:', result);
   };
 
-   //========== main onRecord function========// 
-
-
-
+  //========== main onRecord function========//
 
   onStopRecord = async () => {
     const result = await this.audioRecorderPlayer.stopRecorder();
@@ -62,16 +50,18 @@ export class ClassRecord extends Component {
     console.log('onStartPlay');
     const msg = await this.audioRecorderPlayer.startPlayer();
     console.log(msg);
-    this.audioRecorderPlayer.addPlayBackListener((e) => {
+    this.audioRecorderPlayer.addPlayBackListener(e => {
       this.setState({
         currentPositionSec: e.currentPosition,
         currentDurationSec: e.duration,
-        playTime: this.audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+        playTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.currentPosition),
+        ),
         duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
       });
       return;
     });
-  }
+  };
   onPausePlay = async () => {
     await this.audioRecorderPlayer.pausePlayer();
   };
@@ -82,24 +72,52 @@ export class ClassRecord extends Component {
   };
   render() {
     return (
-      <View style={{flex:1,backgroundColor:'#EFE5E6',justifyContent:'center',alignItems:'center'}}>
-      <View style={{backgroundColor:'coral',height:150,width:150,justifyContent:'center',borderRadius:100,alignItems:'center'}}>
-      <Text style={{fontSize:18,color:'white',textAlign:'center'}}>Time:{this.state.recordTime}</Text>
-      </View>
-      
-       <Button title='Record' onPress={() => this.onStartRecord()}/>
-       <Button title='Stop'  onPress={() => this.onStopRecord()}/>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#EFE5E6',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            backgroundColor: 'coral',
+            height: 150,
+            width: 150,
+            justifyContent: 'center',
+            borderRadius: 100,
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 18, color: 'white', textAlign: 'center'}}>
+            Time:{this.state.recordTime}
+          </Text>
+        </View>
+        <View style={{margin: 10}}>
+          <Button title="Record" onPress={() => this.onStartRecord()} />
+        </View>
 
-       <View style={{backgroundColor:'coral',height:150,width:150,justifyContent:'center',borderRadius:100,alignItems:'center'}}>
-      <Text style={{fontSize:18,color:'white',textAlign:'center'}}>{this.state.playTime} / {this.state.duration}</Text>
-      </View>
+        <Button title="Stop" onPress={() => this.onStopRecord()} />
 
-       <Button title='Play' onPress={() => this.onStartPlay()}/>
-       <Button title='Pause' onPress={() => this.onPausePlay()}/>
-       <Button title='Stop'  onPress={() => this.onStopPlay()}/>
+        <View
+          style={{
+            backgroundColor: 'coral',
+            height: 150,
+            width: 150,
+            justifyContent: 'center',
+            borderRadius: 100,
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 18, color: 'white', textAlign: 'center'}}>
+            {this.state.playTime} / {this.state.duration}
+          </Text>
+        </View>
+
+        <Button title="Play" onPress={() => this.onStartPlay()} />
+        <Button title="Pause" onPress={() => this.onPausePlay()} />
+        <Button title="Stop" onPress={() => this.onStopPlay()} />
       </View>
-    )
+    );
   }
 }
 
-export default ClassRecord
+export default ClassRecord;
